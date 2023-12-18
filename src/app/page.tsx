143b605e255cd20c8afd3b500c4b7c141c1772b2
I.B. Dwi <hi@ibedwi.dev>
2023-12-18 20:30:48 +0700

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import { Fan } from "@/features/fan/Fan";
-import Image from "next/image";
+import Link from "next/link";
 
+const PAGES = [
+  {
+    title: "0. Fan machine",
+    href: "/fan",
+  },
+  {
+    title: "1. Phone keypad machine",
+    href: "/phone-keypad",
+  },
+];
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Fan />
+    <main className="flex min-h-screen flex-col items-start justify-start p-24">
+      <h1 className="text-2xl font-bold">Machine List:</h1>
+      {PAGES.map((page, index) => (
+        <Link
+          className="underline hover:opacity-40"
+          key={index}
+          href={page.href}
+        >
+          {page.title}
+        </Link>
+      ))}
     </main>
   );
 }
